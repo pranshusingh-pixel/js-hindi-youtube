e4c7222b2d8896ba3f9ce83a31c5ba645bedc7c6
pranshusingh-pixel <pranshusingh727@gmail.com>
2026-02-17 06:19:47 +0000

    datatypes

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";   // treat all JS code as newer version of javaScript
+
+// alert(3 + 3)  // gives error because we are using nodejs, not browser and alert needs some more declaration for this type of operation
+
+console.log(3 + 3)   // code readability should be high
+
+// console.log(3 + 3); console.log("hitesh")  // code readability is low in this
+
+console.log("Hitesh")
+
+let name = "Pranshu"
+let age = 20
+let isLoggedIn = false
+
+// <- MORE DATATYPES ->
+// number => 2 to power 53  -> range
+// bigint - for large numbers
+// string => " " -> use in double codes
+// boolean => true/false
+// null => standalone value -> let state = null (we confirm that it is empty)
+// undefined => ex. let state; -> now value is not assigned
+// symbol =>unique
+
+
+// OBJECT 
+
+console.log(typeof "pranshu");  // string
+console.log(typeof "undefined");  // undefined
+console.log(typeof "null");  // object
+console.log(typeof "age");  // number
